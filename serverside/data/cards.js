227c39e7d9a01c1c4edee3cf42c79e07a560668a
maryapple <maria9ku@gmail.com>
2019-11-16 10:23:50 +0300

    added cards.js

---
+++ serverside/data/cards.js
@@ -0,0 +1,37 @@
+module.exports = [
+    {
+        "name": "Classic",
+        "type": "debit",
+        "cashback": "3%",
+        "cost of service per year, rub": "700",
+        "payment system": ["visa", "mastercard", "мир"]
+    },
+    {
+        "name": "Super Cashback",
+        "type": "debit",
+        "cashback": "20%",
+        "cost of service per year, rub": "700",
+        "payment system": ["visa", "mastercard", "мир"]
+    },
+    {
+        "name": "Visa Gold",
+        "type": "debit",
+        "cashback": "5%",
+        "cost of service per year, rub": "3000",
+        "payment system": "visa"
+    },
+    {
+        "name": "Junior",
+        "type": "debit",
+        "cashback": "3%",
+        "cost of service per year, rub": "500",
+        "payment system": ["visa", "mastercard", "мир"]
+    },
+    {
+        "name": "Aerolines Card",
+        "type": ["debit", "credit"],
+        "cashback": "1%",
+        "cost of service per year, rub": "1000",
+        "payment system": ["visa", "mastercard", "мир"]
+    }
+]
