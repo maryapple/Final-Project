e4779fed7aa8b060eaab781b498d2fd6e19380ce
maryapple <maria9ku@gmail.com>
2019-11-20 19:28:42 +0300

    Renamed cards files

--- serverside/routes/index.routes.js
+++ serverside/routes/index.routes.js
@@ -3,5 +3,5 @@ const router = express.Router()
 module.exports = router
 
 router.use('/api/users', require('./user.routes'))
-router.use('/api/cards', require('./cards.routes'))
+router.use('/api/cards', require('./card.routes'))
 router.use('/api/accounts', require('./account.routes'))
