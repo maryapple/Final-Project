193f961c255e0e68c63772d4610d36ae43073842
maryapple <maria9ku@gmail.com>
2019-11-20 21:30:17 +0300

    Navbar styled

--- clientside/src/components/nav/Nav.js
+++ clientside/src/components/nav/Nav.js
@@ -33,20 +33,32 @@ const Nav = () => {
         <div className="sidebar-container">
             <nav className="sidebar">
                 <div className="sidebar-header">
-                    <h3>My Mini Bank</h3>
+                    <h3>My Bank</h3>
                 </div>
-                <ul className="list-unstyled components">
+                <ul>
                     <li>
-                        <a href="#" data-toggle="collapse" aria-expanded="false">
-                            <i className="fas fa-home fa-5x"></i>
+                        {/* <a href="#">
+                            <i className="fas fa-home fa-3x"></i>
+                            home
+                        </a> */}
+                        <div className="li-content">
+                            <div className="li-icon">
+                                <i className="fas fa-home fa-3x"></i>
+                            </div>
+                            {/* <div className="li-text">
+                                home
+                            </div> */}
+                            
+                        </div>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i className="fas fa-user fa-3x"></i>
                         </a>
                     </li>
                     <li>
-                        <a href="#"><i className="fas fa-user fa-5x"></i></a>
-                    </li>
-                    <li>
-                        <a href="#" data-toggle="collapse" aria-expanded="false">
-                            <i className="fas fa-cog fa-5x"></i>
+                        <a href="#">
+                            <i className="fas fa-cog fa-3x"></i>
                         </a>
                     </li>
                 </ul>
--- clientside/src/components/panel/Panel.js
+++ clientside/src/components/panel/Panel.js
@@ -6,8 +6,8 @@ const Panel = () => {
         <div className="panel-container">
             <div className="btn-group" role="group" id="panel-group">
                 <button type="button" className="btn btn-secondary btn-lg">Left</button>
-                <button type="button" class="btn btn-secondary btn-lg">Middle</button>
-                <button type="button" class="btn btn-secondary btn-lg">Right</button>
+                <button type="button" className="btn btn-secondary btn-lg">Middle</button>
+                <button type="button" className="btn btn-secondary btn-lg">Right</button>
             </div >
         </div>
     )
