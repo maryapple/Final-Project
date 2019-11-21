4c5f11af17a73c385d7c67744b457c963f605c38
maryapple <maria9ku@gmail.com>
2019-11-21 07:15:27 +0300

    Added component UserInfo

--- clientside/src/components/app/App.js
+++ clientside/src/components/app/App.js
@@ -3,13 +3,18 @@ import './App.css'
 import Nav from '../nav'
 import {Route, Switch} from 'react-router-dom'
 import Panel from '../panel'
+import UserInfo from '../user-info'
 
 export default class App extends Component {
 	render() {
 		return (
 			<div className="main-container">
 				<Nav />
-				<Panel />
+				<div className="main-page-container">
+					<Panel />
+					<UserInfo />
+				</div>
+				
 			</div>
 		)
 	}
---
+++ clientside/src/components/user-info/UserInfo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './UserInfo.css'
+
+const UserInfo = () => {
+    return (
+        <div className="user-info-container">
+            <h1>Иванов Иван</h1>
+            <span>Счет №</span>
+            <span>Карта №</span>
+        </div>
+    )
+}
+
+export default UserInfo
