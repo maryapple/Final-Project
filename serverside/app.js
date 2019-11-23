87a0c1b29498444d967b46f06cb112c5e120e500
maryapple <maria9ku@gmail.com>
2019-11-23 15:40:09 +0300

    modified app.js

--- serverside/app.js
+++ serverside/app.js
@@ -8,10 +8,16 @@ app.use(morgan('tiny'))
     res.json({ message: 'Hello world' })
 }) */
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+}
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(require('./routes/index.routes'))
 
-app.listen(1337, () => {
+const PORT = process.env.PORT || 1337
+
+app.listen(PORT, () => {
     console.log('Server is running on port 1337')
 })
