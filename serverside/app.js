c5ebc1cb88ce66e6c82cf36bbb5a1236cbb4b64b
maryapple <maria9ku@gmail.com>
2019-11-28 16:14:28 +0300

    added views

--- serverside/app.js
+++ serverside/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-
+const path = require('path')
 const morgan = require('morgan')
 app.use(morgan('tiny'))
 
@@ -12,9 +12,20 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 }
 
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(require('./routes/index.routes'))
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'index page',
+        message: 'index page',
+        description: 'index page'
+    })
+})
 
 const PORT = process.env.PORT || 1337
 
